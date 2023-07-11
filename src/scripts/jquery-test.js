80235993efde803422e5ed620cbbc60c6d0fa2e7
ysk3da <ysk3da@icloud.com>
2023-07-11 13:32:17 +0900

    サンプルコンポーネントの追加に伴いページも整理した

--- src/scripts/jquery-test.js
+++ src/scripts/jquery-test.js
@@ -1,4 +1,14 @@
 import $ from 'jquery';
 
-$('main').append('Jquery');
 console.log('use jquery');
+
+jqueryTestTunc();
+function jqueryTestTunc() {
+  const targetElement = $('#jquery-elm');
+  // jQueryの場合は要素のlengthで判定する
+  if(targetElement.length > 0){
+    targetElement.append('Jquery appended this text!');
+  } else {
+    console.log('targetElement is missing.');
+  }
+}
--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,3 +1,3 @@
 export const Test = () => {
-  console.log('test');
+  console.log('This is Test in test.ts');
 }
