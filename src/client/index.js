d7e8c2211c7a892e527eb7573f5895e1f058f289
ysk3da <ysk3da@icloud.com>
2023-10-03 10:15:48 +0900

    add-slider

--- src/client/index.js
+++ src/client/index.js
@@ -4,3 +4,10 @@ import "../scripts/jquery-test.js";
 
 Test();
 reloadOnResized();
+
+// ユーザー入力を文字列として受け取る
+const input = window.prompt("数字を入力してください", "42");
+// 文字列を数値に変換する
+const num = Number(input);
+console.log(typeof num); // => "number"
+console.log(num); // 入力された文字列を数値に変換したもの
